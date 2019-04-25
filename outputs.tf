8f33e95e37339e23f1ef4d078c80ee9e7895e986
Dean Illfeld <dean.illfeld@cmdsolutions.com.au>
2019-04-26 09:57:47 +1000

    add tier outputs

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,11 @@
 output "public_tier_subnet" {
   value = local.public_tier_subnet
 }
+
+output "private_tier_subnet" {
+  value = local.private_tier_subnet
+}
+
+output "secure_tier_subnet" {
+  value = local.private_tier_subnet
+}
