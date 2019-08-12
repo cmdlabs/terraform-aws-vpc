7f5ed0228465657a69f2195cc21ad457a0788040
Aaron Gorka <aaron.gorka@cmdsolutions.com.au>
2019-08-12 12:05:45 +1000

    Run terraform fmt .

--- routes-secure.tf
+++ routes-secure.tf
@@ -14,14 +14,14 @@ resource "aws_route_table" "secure" {
 resource "aws_route_table_association" "secure" {
   count = length(var.availability_zones)
 
-  subnet_id = aws_subnet.secure[count.index].id
+  subnet_id      = aws_subnet.secure[count.index].id
   route_table_id = aws_route_table.secure[count.index].id
 }
 
 resource "aws_route" "secure_default" {
   count = var.enable_nat_gateway ? length(var.availability_zones) : 0
 
-  route_table_id = aws_route_table.secure[count.index].id
+  route_table_id         = aws_route_table.secure[count.index].id
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id = var.enable_per_az_nat_gateway ? aws_nat_gateway.main[count.index].id : aws_nat_gateway.main[0].id
+  nat_gateway_id         = var.enable_per_az_nat_gateway ? aws_nat_gateway.main[count.index].id : aws_nat_gateway.main[0].id
 }
