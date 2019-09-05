d866b3a9060f7d1cdf26becb61c6dbaf7b4e048f
Anish Muraleedharan <anish.muraleedharan@cmdsolutions.com.au>
2019-09-05 11:54:01 +1000

    corrected endpoint template

--- endpointecr.tf
+++ endpointecr.tf
@@ -25,8 +25,8 @@ resource "aws_vpc_endpoint" "ecr_dkr_endpoint" {
   vpc_id = aws_vpc.main.id
   service_name = "com.amazonaws.ap-southeast-2.ecr.dkr"
   vpc_endpoint_type = "Interface"
-  #security_group_ids = ["sg-bf9ce5c4"]
-  subnet_ids         = ["aws_subnet.private.id","aws_subnet.secure.id"]
+  security_group_ids = ["${aws_security_group.sgforecrendpoint.id}"]
+  subnet_ids         = ["${aws_subnet.private.id}","${aws_subnet.secure.id}"]
   private_dns_enabled = true
   tags = {
     Environment = "test",
@@ -38,8 +38,8 @@ resource "aws_vpc_endpoint" "ecr_api_endpoint" {
   vpc_id = aws_vpc.main.id
   service_name = "com.amazonaws.ap-southeast-2.ecr.api"
   vpc_endpoint_type = "Interface"
-  #security_group_ids = ["sg-bf9ce5c4"]
-  subnet_ids         = ["aws_subnet.private.id","aws_subnet.secure.id"]
+  security_group_ids = ["${aws_security_group.sgforecrendpoint.id}"]
+  subnet_ids         = ["${aws_subnet.private.id}","${aws_subnet.secure.id}"]
   private_dns_enabled = true
   tags = {
     Environment = "test",
