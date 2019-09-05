3269d2f86e088e0cacaa940900273b65f070748e
Anish Muraleedharan <anish.muraleedharan@cmdsolutions.com.au>
2019-09-05 11:47:16 +1000

    added security groups in endpoints.tf

--- endpointecr.tf
+++ endpointecr.tf
@@ -1,3 +1,26 @@
+resource "aws_security_group" "sgforecrendpoint" {
+  name        = "ECREndpointSG"
+  description = "Allow indbound and outbound traffic for ecr endpoint"
+  vpc_id      = "${aws_vpc.main.id}"
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
+
+
 resource "aws_vpc_endpoint" "ecr_dkr_endpoint" {
   vpc_id = aws_vpc.main.id
   service_name = "com.amazonaws.ap-southeast-2.ecr.dkr"
