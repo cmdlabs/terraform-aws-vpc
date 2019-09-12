439108fdff13d2239d4aa5b4cd74185e483c6074
Anish Muraleedharan <anish.muraleedharan@cmdsolutions.com.au>
2019-09-12 11:49:23 +1000

    Added sec-group-rule resource and remove few comments

--- vpc-endpoints.tf
+++ vpc-endpoints.tf
@@ -2,31 +2,32 @@ resource "aws_security_group" "sgforendpoint" {
   name        = "EndpointSG"
   description = "Allow indbound and outbound traffic for VPC endpoint"
   vpc_id      = "${aws_vpc.main.id}"
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    # Please restrict your ingress to only necessary IPs and ports.
-    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
 }
 
+resource "aws_security_group_rule" "allow_all_ingress" {
+  type            = "ingress"
+  from_port       = 0
+  to_port         = 0
+  protocol        = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.sgforendpoint.id}"
+}
+
+resource "aws_security_group_rule" "allow_all_egress" {
+  type            = "egress"
+  from_port       = 0
+  to_port         = 0
+  protocol        = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = "${aws_security_group.sgforendpoint.id}"
+}
 
 resource "aws_vpc_endpoint" "vpc_endpoint" {
   for_each            = toset(var.vpc_endpoints)
   vpc_id              = aws_vpc.main.id
   service_name        = "com.amazonaws.${data.aws_region.current.name}.${each.value}"
   vpc_endpoint_type   = "Interface"
-  security_group_ids  = ["${aws_security_group.sgforendpoint.id}"]
+  security_group_ids  = [aws_security_group.sgforendpoint.id]
   subnet_ids          = aws_subnet.private.*.id
   tags = merge(
     { Name = "${var.vpc_name}-${each.value}-endpoint" },
